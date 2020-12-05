ea91dfab9f841a49651f05193cf73fa8df9de443
Spuckwaffel <68144475+Spuckwaffel@users.noreply.github.com>
2020-12-05 15:30:22 +0100

    Add files via upload

---
+++ Amongusoveraly/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Amongusoveraly")]
+[assembly: AssemblyDescription("a among us ovverlay with gui")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Spuckwaffel")]
+[assembly: AssemblyProduct("Amongusoveraly")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Spuckwaffel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c6ebd65-ac6e-4837-8b82-1da2c10a1c70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("1.2.3.4")]
---
+++ Amongusoveraly/resc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amongusoveraly
+{
+    public class resc
+    {
+        public static bool firstrun = true;
+        public static int MainRadarX = 0;
+        public static int MainRadarY = 0;
+        public static int MainNameX = 0;
+        public static int MainNameY = 0;
+        public static int GUITPX = 0;
+        public static int GUITPY = 0;
+        public static int GUIShortcutsX = 0;
+        public static int GUIShortcutsY = 0;
+        public static int GUIHostX = 0;
+        public static int GUIHostY = 0;
+        public static int GUICheatsX = 0;
+        public static int GUICheatsY = 0;
+        public static bool GUIradarClosed = false;
+        public static bool GUINameClosed = false;
+        public static bool GUITPClosed = false;
+        public static bool GUIShortcutsClosed = false;
+        public static bool GUIHostClosed = false;
+        public static bool HideRadar = false;
+        public static bool HidePlayer = false;
+        public static bool GUICheatsClosed = false;
+        public static bool killprogram = false;
+    }
+}
